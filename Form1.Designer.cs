8e7cce3ea18171161234e4ccdcb3c6ecd2ac6a60
Bartosz Zalewski <80588334+Deszynski@users.noreply.github.com>
2021-04-17 15:55:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Grafika_Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Swap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 491);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Negative";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jasność";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "wybór trybu mieszania";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(212, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(412, 370);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(646, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(414, 372);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 94);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(178, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 226);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(165, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 532);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafika_Lab5
+{
+    public partial class Form1 : Form
+    {
+        private int width = 0;
+        private int height = 0;
+        private int jasnosc = 0;
+        private Image temp;
+
+        private int[] red = new int[256];
+        private int[] green = new int[256];
+        private int[] blue = new int[256];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonsActive(false);
+
+            // Tryby mieszania
+            comboBox1.Items.Add("Suma");
+            comboBox1.Items.Add("Odejmowanie");
+            comboBox1.Items.Add("Różnica");
+            comboBox1.Items.Add("Mnożenie");
+            comboBox1.Items.Add("Mnożenie Odwrotności");
+            comboBox1.Items.Add("Negacja");
+            comboBox1.Items.Add("Ciemniejsze");
+            comboBox1.Items.Add("Jaśniejsze");
+            comboBox1.Items.Add("Wyłączenie");
+            comboBox1.Items.Add("Nakładka");
+            comboBox1.Items.Add("Ostre Światło");
+            comboBox1.Items.Add("Łagodne Światło");
+            comboBox1.Items.Add("Rozcieńczenie");
+            comboBox1.Items.Add("Wypalanie");
+            comboBox1.Items.Add("Reflect Mode");
+            comboBox1.Items.Add("Przeroczystość");
+        }
+
+        //load
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+
+                width = pictureBox1.Image.Width;
+                height = pictureBox1.Image.Height;
+
+                if (temp == null) pictureBox2.Image = new Bitmap(width, height);
+                else pictureBox2.Image = temp;
+
+                if (pictureBox1.Image.Width != pictureBox2.Image.Width && pictureBox1.Image.Height != pictureBox2.Image.Height) buttonsActive(false);
+                else buttonsActive(true);
+            }
+        }
+
+        //swap
+        private void button2_Click(object sender, EventArgs e)
+        {
+            temp = pictureBox2.Image;
+            pictureBox2.Image = pictureBox1.Image;
+            pictureBox1.Image = temp;
+            temp = pictureBox2.Image;
+        }
+
+        //negative
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            Color k;
+            int r, g, b;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    k = b1.GetPixel(x, y);
+                    r = 255 - k.R;
+                    g = 255 - k.G;
+                    b = 255 - k.B;
+
+                    k = Color.FromArgb(r, g, b);
+
+                    b2.SetPixel(x, y, k);
+                }
+            }
+
+            pictureBox2.Refresh();
+        }
+
+        private void buttonsActive(Boolean b)
+        {
+            button2.Enabled = b; // zamiana stron
+            button3.Enabled = b; // negatyw
+            comboBox1.Enabled = b; // tryb mieszania
+            trackBar1.Enabled = b; // jasnosc
+        }
+
+        //jasnosc
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            jasnosc = trackBar1.Value;
+
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            Color k;
+            int r, g, b;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    k = b1.GetPixel(x, y);
+
+                    if (jasnosc + k.R > 255) r = 255;
+                    else if (jasnosc + k.R < 0) r = 0;
+                    else r = jasnosc + k.R;
+
+                    if (jasnosc + k.G > 255) g = 255;
+                    else if (jasnosc + k.G < 0) g = 0;
+                    else g = jasnosc + k.G;
+
+                    if (jasnosc + k.B > 255) b = 255;
+                    else if (jasnosc + k.B < 0) b = 0;
+                    else b = jasnosc + k.B;
+
+                    k = Color.FromArgb(r, g, b);
+
+                    b2.SetPixel(x, y, k);
+                }
+            }
+
+            pictureBox2.Refresh();
+        }
+
+        //tryby
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)pictureBox1.Image;
+            Bitmap b2 = (Bitmap)pictureBox2.Image;
+
+            Color k, k1, k2;
+            int r, g, b;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    k1 = b1.GetPixel(x, y);
+                    k2 = b2.GetPixel(x, y);
+
+                    if (comboBox1.Text == "Suma")
+                    {
+                        if (k1.R + k2.R > 255) r = 255;
+                        else r = k1.R + k2.R;
+
+                        if (k1.G + k2.G > 255) g = 255;
+                        else g = k1.G + k2.G;
+
+                        if (k1.B + k2.B > 255) b = 255;
+                        else b = k1.B + k2.B;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Odejmowanie")
+                    {
+                        if (k1.R - k2.R < 0) r = 0;
+                        else r = k1.R - k2.R;
+
+                        if (k1.G - k2.G < 0) g = 0;
+                        else g = k1.G - k2.G;
+
+                        if (k1.B - k2.B < 0) b = 0;
+                        else b = k1.B - k2.B;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Różnica")
+                    {
+                        r = k1.R - k2.R;
+                        if (r < 0) r *= -1;
+
+                        g = k1.G - k2.G;
+                        if (g < 0) g *= -1;
+
+                        b = k1.B - k2.B;
+                        if (b < 0) b *= -1;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Mnożenie")
+                    {
+                        r = k1.R * k2.R / 255;
+
+                        g = k1.G * k2.G / 255;
+
+                        b = k1.B * k2.B / 255;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Mnożenie Odwrotności")
+                    {
+                        r = 255 - (((255 - k1.R) * (255 - k2.R)) / 255);
+                        if (r > 255) r = 255;
+                        if (r < 0) r = 0;
+
+                        g = 255 - (((255 - k1.G) * (255 - k2.G)) / 255);
+                        if (g > 255) g = 255;
+                        if (g < 0) g = 0;
+
+                        b = 255 - (((255 - k1.B) * (255 - k2.B)) / 255);
+                        if (b > 255) b = 255;
+                        if (b < 0) b = 0;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Negacja")
+                    {
+                        r = 255 - k1.R - k2.R;
+                        if (r < 0) r *= -1;
+                        r = 255 - r;
+
+                        g = 255 - k1.G - k2.G;
+                        if (g < 0) g *= -1;
+                        g = 255 - g;
+
+                        b = 255 - k1.B - k2.B;
+                        if (b < 0) b *= -1;
+                        b = 255 - b;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Ciemniejsze")
+                    {
+                        if (k1.R < k2.R) r = k1.R;
+                        else r = k2.R;
+
+                        if (k1.G < k2.G) g = k1.G;
+                        else g = k2.G;
+
+                        if (k1.B < k2.B) b = k1.B;
+                        else b = k2.B;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Jaśniejsze")
+                    {
+                        if (k1.R > k2.R) r = k1.R;
+                        else r = k2.R;
+
+                        if (k1.G > k2.G) g = k1.G;
+                        else g = k2.G;
+
+                        if (k1.B > k2.B) b = k1.B;
+                        else b = k2.B;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Wyłączenie")
+                    {
+                        r = k1.R + k2.R - (2 * k1.R * k2.R) / 255;
+                        if (r < 0) r *= -1;
+
+                        g = k1.G + k2.G - (2 * k1.G * k2.G) / 255;
+                        if (g < 0) g *= -1;
+
+                        b = k1.B + k2.B - (2 * k1.B * k2.B) / 255;
+                        if (b < 0) b *= -1;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Nakładka")
+                    {
+                        if (k1.R < 128) r = (2 * k1.R * k2.R) / 255;
+                        else r = 255 - (2 * (255 - k1.R) * (255 - k2.R)) / 255;
+                        if (r < 0) r *= -1;
+
+                        if (k1.G < 128) g = (2 * k1.G * k2.G) / 255;
+                        else g = 255 - (2 * (255 - k1.G) * (255 - k2.G)) / 255;
+                        if (g < 0) g *= -1;
+
+                        if (k1.B < 128) b = (2 * k1.B * k2.B) / 255;
+                        else b = 255 - (2 * (255 - k1.B) * (255 - k2.B)) / 255;
+                        if (b < 0) b *= -1;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Ostre Światło")
+                    {
+                        if (k2.R < 0) r = (2 * k1.R * k2.R) / 255;
+                        else r = 255 - (2 * (255 - k1.R) * (255 - k2.R)) / 255;
+                        if (r < 0) r *= -1;
+
+                        if (k2.G < 0) g = (2 * k1.G * k2.G) / 255;
+                        else g = 255 - (2 * (255 - k1.G) * (255 - k2.G)) / 255;
+                        if (g < 0) g *= -1;
+
+                        if (k2.B < 0) b = (2 * k1.B * k2.B) / 255;
+                        else b = 255 - (2 * (255 - k1.B) * (255 - k2.B)) / 255;
+                        if (b < 0) b *= -1;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Łagodne Światło")
+                    {
+                        if (k2.R < 0.5) r = ((2 * k1.R * k2.R) / 255) + ((((k1.R * k1.R) / 255) * (255 - (k2.R * k2.R) / 255)) / 255);
+                        else r = ((k1.R * (((k2.R * 2) / 255) - 1)) / 255) + ((((k1.R * 2) / 255) * (255 - k2.R)) / 255);
+                        if (r < 0) r *= -1;
+
+                        if (k2.G < 0.5) g = ((2 * k1.G * k2.G) / 255) + ((((k1.G * k1.G) / 255) * (255 - (k2.G * k2.G) / 255)) / 255);
+                        else g = ((k1.G * (((k2.G * 2) / 255) - 1)) / 255) + ((((k1.G * 2) / 255) * (255 - k2.G)) / 255);
+                        if (g < 0) g *= -1;
+
+                        if (k2.B < 0.5) b = ((2 * k1.B * k2.B) / 255) + ((((k1.B * k1.B) / 255) * (255 - (k2.B * k2.B) / 255)) / 255);
+                        else b = ((k1.B * (((k2.B * 2) / 255) - 1)) / 255) + ((((k1.B * 2) / 255) * (255 - k2.B)) / 255);
+                        if (b < 0) b *= -1;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Rozcieńczenie")
+                    {
+                        if (k2.R == 0) r = 255;
+                        else r = (k1.R * 255 / (255 - k2.R));
+                        if (r < 0) r = 0; if (r > 255) r = 255;
+
+                        if (k2.G == 0) g = 255;
+                        else g = (k1.G * 255 / (255 - k2.G));
+                        if (g < 0) g = 0; if (g > 255) g = 255;
+
+                        if (k2.B == 0) b = 255;
+                        else b = (k1.B * 255 / (255 - k2.B));
+                        if (b < 0) b = 0; if (b > 255) b = 255;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                    else if (comboBox1.Text == "Wypalanie")
+                    {
+                        if (k2.R == 0) r = 255;
+                        else r = (255 - (((255 - k1.R) * 255) / k2.R));
+                        if (r < 0) r = 0; if (r > 255) r = 255;
+
+                        if (k2.G == 0) g = 255;
+                        else g = (255 - (((255 - k1.G) * 255) / k2.G));
+                        if (g < 0) g = 0; if (g > 255) g = 255;
+
+                        if (k2.B == 0) b = 255;
+                        else b = (255 - (((255 - k1.B) * 255) / k2.B));
+                        if (b < 0) b = 0; if (b > 255) b = 255;
+
+                        k = Color.FromArgb(r, g, b);
+                        b2.SetPixel(x, y, k);
+                    }
+                }
+            }
+
+            pictureBox2.Refresh();
+        }
+    }
+}
